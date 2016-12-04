73937aff3095076b0e2e151e0da9f61bf4c68c13
Bart Jansen <b@rtjansen.nl>
2016-12-04 23:08:22 +0100

    initial version instacognitive

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
 var $ = require('jquery');
 
 $(document).ready(function(){
+    $('header .mobile .burger').on('click', function() {
+        $('ul.mobile-dropdown').toggleClass('active');
+    });
     console.log('successfully loaded');
 });
--- js/bundle.js
+++ js/bundle.js
@@ -4,6 +4,9 @@
 var $ = require('jquery');
 
 $(document).ready(function () {
+    $('header .mobile .burger').on('click', function () {
+        $('ul.mobile-dropdown').toggleClass('active');
+    });
     console.log('successfully loaded');
 });
 
